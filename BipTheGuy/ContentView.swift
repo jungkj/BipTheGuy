07863858259f1eb29ea2c810f5e6c3048fa79c5b
Andy Jung <jungkj@bc.edu>
2023-02-19 19:45:50 -0500

    Week5 HW Andy Jung

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,16 +6,77 @@
 //
 
 import SwiftUI
+import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
+    
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var animateImage = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
+    
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            
+            
+            
+            bipImage
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                    animateImage = false
+                    withAnimation (.spring(response: 0.3, dampingFraction: 0.3)){
+                        animateImage = true // will go from 90% size to 100% but using the .spring animation
+                    }
+                }
+               
+            
+            
+            Spacer()
+            
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+            .onChange(of: selectedPhoto) { newValue in
+                Task{
+                    do{
+                        if let data = try await newValue?.loadTransferable(type: Data.self){
+                            if let uiImage = UIImage(data: data){
+                                bipImage = Image(uiImage: uiImage)
+                            }
+
+                        }
+                    } catch{
+                        print("😡 ERROR: loading failed \(error.localizedDescription)")
+                    }
+                }
+            }
+
+
+                
+
         }
         .padding()
+    }
+    func playSound(soundName: String){
+        guard let soundFile = NSDataAsset(name: soundName)else{
+            print("😡 Could not return \(soundName)")
+            return
+        }
+        
+        
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        }catch{
+            print("😡 ERROR: \(error.localizedDescription)")
+        }
     }
 }
 
